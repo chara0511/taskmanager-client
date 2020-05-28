5ee96fc0e95fd9409d4792ddbc52638e5ad213a9
Chara- <escor05_11@hotmail.com>
2020-05-27 20:03:17 -0500

    adding routes

---
+++ src/components/auth/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <h1>from Login</h1>;
+};
+
+export default Login;
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <h1>from Projects</h1>;
+};
+
+export default Projects;
